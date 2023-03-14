c52f4089b5db22e20e4d62be165f5e1e4cd5e861
GabrielMorin1109 <gabriel.morin1109@outlook.com>
2023-03-14 17:14:07 -0400

    Ignore change in folder Data

--- Bash/download_IBTrACS.bash
+++ Bash/download_IBTrACS.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # make the first argument when calling the script as file_path variable
-file_path = $1 
+file_path = Data/Data_IBTrACS/
 
 # get IBTrACS and save it to file_dir
 wget \
@@ -8,7 +8,7 @@ wget \
     https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.ACTIVE.list.v04r00.csv
 
 # create MSWEP_FileName.txt to save 
-echo "FileName" > Data/MSWEP_FileName.txt
+echo "FileName" > Data/Data_IBTrACS/MSWEP_FileName.txt
 
 # list all file, select only filename with .nc, append filename into MSWEP_FileName.txt 
-rclone ls -v --drive-shared-with-me "GoogleDrive:/MSWEP_V280/Past/3hourly" | grep ".nc" > Data/MSWEP_FileName.txt
+rclone ls -v --drive-shared-with-me "GoogleDrive:/MSWEP_V280/Past/3hourly" | grep ".nc" > Data/Data_IBTrACS/MSWEP_FileName.txt
