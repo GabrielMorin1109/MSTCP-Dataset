dd303db01191f036655b4f6678c9f7f2926fa343
GabrielMorin1109 <gabriel.morin1109@outlook.com>
2023-04-04 15:27:03 -0400

    Script that compute the grid cell area of a raster

---
+++ bin/grid_area_creation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#%%
+from os import walk, path, system
+from pyprojroot.here import here
+
+MSWEP_folder = here("Data/Data_MSWEP")
+MSWEP_paths = [
+    path.join(dirpath, f)
+    for (dirpath, dirnames, filenames) in walk(MSWEP_folder)
+    for f in filenames
+]
+# select one file
+MSWEP_path = MSWEP_paths[0]
+# %%
+# create the grid cell area file
+command = f"cdo gridarea {MSWEP_path} {MSWEP_folder}/grid_area.nc"
+system(command)
